85ad4755edaa640e587a7519c05335d3e425fbfa
Gawain Lynch <gawain.lynch@gmail.com>
2015-11-02 03:54:13 +0000

    Move asset handling to middleware

--- Extension.php
+++ Extension.php
@@ -2,35 +2,31 @@
 
 namespace Bolt\Extension\Bolt\Socialite;
 
+use Bolt\BaseExtension;
 use Bolt\Extensions\Snippets\Location as SnippetLocation;
 
 /**
- * HTML element footprint based on file time
+ * Socialite extension for Bolt.
  *
  * @author Gawain Lynch <gawain.lynch@gmail.com>
  */
-class Extension extends \Bolt\BaseExtension
+class Extension extends BaseExtension
 {
     public function getName()
     {
-        return "Socialite";
+        return 'Socialite';
     }
 
     public function initialize()
     {
-        /*
-         * Frontend
-         */
-        if ($this->app['config']->getWhichEnd() == 'frontend') {
-            // Assets
-            $this->addAssets();
+        // Front-end assets
+        $this->app->before(array($this, 'before'));
 
-            // Add ourselves to the Twig filesystem path
-            $this->app['twig.loader.filesystem']->addPath(__DIR__ . '/assets/');
+        // Add ourselves to the Twig filesystem path
+        $this->app['twig.loader.filesystem']->addPath(__DIR__ . '/assets/');
 
-            // Catch the TWIG function
-            $this->addTwigFunction('socialite', 'twigSocialite');
-        }
+        // Catch the TWIG function
+        $this->addTwigFunction('socialite', 'twigSocialite');
     }
 
     /**
@@ -46,25 +42,20 @@ class Extension extends \Bolt\BaseExtension
         return $this->widget->createWidget($this->app, $this->config, $buttons);
     }
 
-    private function addAssets()
+    public function before()
     {
-        // Define the path to us
-        $this->config['path'] = substr(__DIR__, strlen($this->app['paths']['rootpath']));
-        $this->config['url'] = $this->app['paths']['canonicalurl'];
+        if ($this->app['config']->getWhichEnd() !== 'frontend') {
+            return;
+        }
 
         // If we're set to actviate by scroll, add a class to <body> that gets
         // caught in socialite.load.js
-        if (empty($this->config['activation']) || $this->config['activation'] == 'scroll') {
+        if (empty($this->config['activation']) || $this->config['activation'] === 'scroll') {
             $html = '<script>document.body.className += "socialite-scroll";</script>';
+            $this->addSnippet(SnippetLocation::END_OF_BODY, $html);
         }
 
-        // Insert out JS late so that we are more likely to work with a late
-        // jQuery insertion
-        $html .= '
-            <script defer src="' . str_replace("//","/",$this->app['resources']->getUrl('root') . $this->config['path']) . '/js/bolt.socialite.min.js"></script>
-            ';
-
-        $this->addSnippet(SnippetLocation::END_OF_HTML, $html);
+        $this->addJavascript('js/bolt.socialite.min.js', array('late' => true));
     }
 
     /**
